c8c79a4c777195baca67f5a65a4cacc4da5155a5
Kim Gihong <kihong2424@gmail.com>
2023-12-12 13:23:50 +0900

    Title: 총합 구하기, Time: 110ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231212/총합 구하기/find-the-total.java
+++ 231212/총합 구하기/find-the-total.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner();
+        Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
         int b = sc.nextInt();
@@ -14,6 +14,7 @@ public class Main {
                 sum += i;
             }
         }
+
         System.out.print(sum);
     }
 }
