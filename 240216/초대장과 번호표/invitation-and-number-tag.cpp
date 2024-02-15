35b4a4cb3dd205309605d13a41ddb7e5cafbe291
Kim Gihong <kihong2424@gmail.com>
2024-02-16 06:12:43 +0900

    Title: 초대장과 번호표, Time: 223ms, Memory: 28MB, Status: Passed - CodeTree

--- 240216/초대장과 번호표/invitation-and-number-tag.cpp
+++ 240216/초대장과 번호표/invitation-and-number-tag.cpp
@@ -1,43 +1,63 @@
 #include <iostream>
 #include <unordered_set>
+#include <queue>
+#include <vector>
+
 using namespace std;
 
-int N; // 1 ~ 100,000
-int G; // 2 ~ 250,000
-unordered_set<int> s;
+#define MAX_N 100000
+#define MAX_G 250000
 
-void    one_group()
+int n, g;
+bool invited[MAX_N];
+unordered_set<int> groups[MAX_G];
+vector<int> people_groups[MAX_N];
+queue<int> q;
+int ans;
+
+int	main()
 {
-    int num;
-    int first;
-    int cnt = 0;
-    int candidate;
+	cin >> n >> g;
+	for (int i = 0; i < g; i++)
+	{
+		int s, x;
+		cin >> s;
+		for (int j = 0; j < s; j++)
+		{
+			cin >> x;
+			x--;
+			groups[i].insert(x);
+			people_groups[x].push_back(i);
+		}
+	}
 
-    cin >> num;
-    for (int i = 0; i < num; i++)
-    {
-        int tmp;
-        cin >> tmp;
-        if (s.find(tmp) == s.end())
-            candidate = tmp;
-        else
-            cnt++;
-    }
-    if (cnt == num - 1)
-        s.insert(candidate);
-}
+	q.push(0);
+	invited[0] = true;
+	while (!q.empty())
+	{
+		int x = q.front();
+		q.pop();
+		ans++;
 
-int main() {
-    // input
-    // freopen_s(new FILE*, "input.txt", "r", stdin);
-    cin >> N >> G;
+		// x가 들어있는 그룹에서 x를 지웁니다.
+		// hashset 에는 그룹에서 초대받지 않은 인원만을 남깁니다.
+		for (int i = 0; i < (int)people_groups[x].size(); i++)
+		{
+			int g_num = people_groups[x][i];
 
-    // solve
-    s.insert(1);
-    for (int g = 0; g < G; g++)
-        one_group();
-
-    // output
-    cout << s.size();
-    return 0;
+			// 해당 그룹에서 x를 지웁니다.
+			groups[g_num].erase(x);
+			// 초대받지 않은 인원이 한명밖에 없다면 초대합니다.
+			if ((int)groups[g_num].size() == 1)
+			{
+				int p_num = *(groups[g_num].begin());
+				if (!invited[p_num])
+				{
+					invited[p_num] = true;
+					q.push(p_num);
+				}
+			}
+		}
+	}
+	cout << ans;
 }
