686f22f262785f7651f40834e4166fec78fb88de
Kim Gihong <kihong2424@gmail.com>
2024-04-03 21:02:29 +0900

    Title: 가까운 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240403/가까운 숫자/nearest-number.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int N;
+
+int main() {
+    set<int> s;
+    int MIN = 21e8;
+
+    cin >> N;
+    s.insert(0);
+    for (int i = 0; i < N; i++) {
+        int num;
+        set<int>::iterator before;
+        set<int>::iterator after;
+        set<int>::iterator here;
+        set<int>::iterator rbeg;
+
+        cin >> num;
+        s.insert(num);
+        here = s.find(num);
+        before = --here;
+        here++;
+        after = ++here;
+        --here;
+        rbeg = --(s.end());
+        if ((num - *before) < MIN)
+            MIN = num - *before;
+        if (here != rbeg && (*after - num) < MIN)
+            MIN = *after - num;
+        cout << MIN << ' ';
+    }
+
+    
+    return 0;
+}
