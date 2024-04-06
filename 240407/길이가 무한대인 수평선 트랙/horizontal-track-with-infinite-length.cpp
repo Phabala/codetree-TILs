31bf1d775feeca9e3892878be4ffcb4bfd9d9281
Kim Gihong <kihong2424@gmail.com>
2024-04-07 01:41:22 +0900

    Title: 길이가 무한대인 수평선 트랙, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240407/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+typedef unsigned long long ull;
+
+int N, T;
+set<pair<ull, ull>> s; // 위치, 속력
+
+int main() {
+    cin >> N >> T;
+
+    ull position, speed;
+    for (int i = 0; i < N; i++) {
+        cin >> position >> speed;
+        s.insert(make_pair(position, speed));
+    }
+
+    set<pair<ull, ull>>::iterator it;    
+    it = s.begin();
+    while(true) {
+        pair<ull, ull> pre, suf;
+        pre = *(it++);
+        if (it == s.end())
+            break ;
+        suf = *it;
+        if (pre.second > suf.second && 
+            ((pre.second - suf.second) * T) >= (suf.first - pre.first))
+            s.erase(pre);
+    }
+    cout << s.size();
+    return 0;
+}
+
+/**
+0에 가까이 위치해 있는 한명을 A라고 하고 그 뒤에 있는 사람들을 B, C, D... 라고 했을 때
+B, C, D 중에서 A의 (위치+속력) 값보다 (위치+속력) 값이 작거나 같 사람들은 1분 뒤에 한 그룹이 된다.
+
+if (A.speed > B.speed) -> if (A.speed - B.speed) * T >= B.position - A.position
+-> 한 그룹이 됨 (A를 지움)
+*/
