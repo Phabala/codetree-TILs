4c427a0511029f3a859931c294a56ed40d6070bf
Kim Gihong <kihong2424@gmail.com>
2024-04-15 16:48:55 +0900

    Title: 가장 가까운 점, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240415/가장 가까운 점/nearest-point.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct compare {
+    bool operator()(pair<int, int> left, pair<int, int> right) {
+        if (left.first * left.second > right.first * right.second)
+            return true;
+        else if (left.first > right.first)
+            return true;
+        else if (left.second > right.second)
+            return true;
+        else
+            return false;
+    }
+};
+
+priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq;
+
+int N, M;
+
+int main() {
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        int x, y;
+        cin >> x >> y;
+        pq.push(make_pair(x, y));
+    }
+
+    for (int i = 0; i < M; i++) {
+        pair<int, int> top;
+        top = pq.top();
+        pq.pop();
+        pq.push(make_pair(top.first + 2, top.second + 2));
+    }
+
+    cout << pq.top().first << ' ' << pq.top().second;
+
+    return 0;
+}
