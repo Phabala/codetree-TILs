5a9e31d848637eced5dc04d07130cf46114b362c
Kim Gihong <kihong2424@gmail.com>
2024-05-02 20:29:38 +0900

    Title: 중앙값, Time: 231ms, Memory: 1MB, Status: Passed - CodeTree

--- 240502/중앙값/median.cpp
+++ 240502/중앙값/median.cpp
@@ -1,29 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
 
-vector<int> v;
-
+priority_queue<int, vector<int>, less<int> > MAX_HEAP;
+priority_queue<int, vector<int>, greater<int> > MIN_HEAP;
+int benchmark;
 int T;
 int N;
 
 int main() {
+    // freopen_s(new FILE*, "input.txt", "r", stdin);
     cin >> T;
+    int num;
     for (int tc = 1; tc <= T; tc++) {
-        v.clear();
+        MAX_HEAP = priority_queue<int, vector<int>, less<int> >();
+        MIN_HEAP = priority_queue<int, vector<int>, greater<int> >();
         cin >> N;
-        for (int i = 1; i <= N; i++) {
-            int num;
+        cin >> benchmark;
+        cout << benchmark << ' ';
+        for (int i = 2; i <= N; i++) {
             cin >> num;
-            v.push_back(num);
-            if (i % 2 == 1) {
-                sort(v.begin(), v.end());
-                cout << v[i / 2] << ' ';
+            if (num < benchmark)
+                MAX_HEAP.push(num);
+            else
+                MIN_HEAP.push(num);
+            if (MAX_HEAP.size() > MIN_HEAP.size())
+            {
+                MIN_HEAP.push(benchmark);
+                benchmark = MAX_HEAP.top();
+                MAX_HEAP.pop();
             }
+            else if (MAX_HEAP.size() < MIN_HEAP.size())
+            {
+                MAX_HEAP.push(benchmark);
+                benchmark = MIN_HEAP.top();
+                MIN_HEAP.pop();
+            }
+            if (i % 2 == 1)
+                cout << benchmark << ' ';
         }
         cout << '\n';
     }
+
     return 0;
 }
