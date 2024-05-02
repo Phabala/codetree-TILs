b2491bc5814f9a795486cce14aa9b093515c1bbd
Kim Gihong <kihong2424@gmail.com>
2024-05-02 18:31:06 +0900

    Title: 앞에서부터 삭제하기 2, Time: 36ms, Memory: 1MB, Status: Passed - CodeTree

--- 240502/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
+++ 240502/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
@@ -1,61 +1,37 @@
+/**
+* 앞에서부터 제거해나간 뒤 (제일 작은 것 하나 뺀) 전체에서 평균의 최소값을 구하는 것은
+* 뒤에서부터 쌓아간 뒤 (제일 작은 것 하나 뺀) 전체에서 평균의 최소값을 구하는 것과 같다.
+*/
+
 #include <iostream>
+#include <vector>
 #include <queue>
-#include <algorithm>
+
+#define MAX_N 100000
 
 using namespace std;
 
-int N;
-int sum = 0, cnt = 0;
-double MAX = -21e8;
-
-vector<int> v;
-priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq; // 값, 순서
-int dict[100001];
+int n;
+int arr[MAX_N];
+int sum_val;
+double max_avg;
+priority_queue<int, vector<int>, greater<int> > pq;
 
 int main()
 {
-	// freopen_s(new FILE*, "input.txt", "r", stdin);
-	cin >> N;
-	cnt = N;
-	int num;
-	for (int i = 1; i <= N; i++) {
-		cin >> num;
-		v.push_back(num);
-		pq.push(make_pair(num, i));
-		sum += num;
-	}
-	pair<int, int> top;
-	for (int k = 1; k <= N - 2; k++) {
-		dict[k]++;
-		cnt--;
-		top = pq.top();
-		pq.pop();
-		while (dict[top.second] == 1)
-		{
-			top = pq.top();
-			pq.pop();
-		}
-		sum -= v[k - 1];
-		sum -= top.first;
-		cnt--;
-		MAX = max(MAX, double(sum / cnt));
-		sum += top.first;
-		cnt++;
-	}
-	cout << fixed;
-	cout.precision(2);
-	cout << MAX;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    pq.push(arr[n - 1]);
+    sum_val += arr[n - 1];
+    for (int i = n - 2; i >= 1; i--) {
+        pq.push(arr[i]);
+        sum_val += arr[i];
+        double avg = (double) (sum_val - (pq.top())) / (n - i - 1);
+        if (max_avg < avg)
+            max_avg = avg;
+    }
+    cout << fixed;
+    cout.precision(2);
+    cout << max_avg;
 }
-
-/**
-* 1. N개의 정수가 주어진다. O(1)
-* 2. 전체 N개의 정수들의 합을 구한다. O(1)
-* 3. 앞에서 숫자 하나를 제거한다. O(logN)
-* 4. 정수들의 합에서 그 숫자만큼 뺀다. O(1)
-* 5. 가장 작은 수 하나를 제거한다. O(logN)
-* 6. 그 수를 정수들의 합에서 뺀다. O(1)
-* 7. N개 정수들의 합에서 빼진 수를 현재 남아있는 수 개수만큼 나눈다. O(1)
-* 8. 아까 뺀 가장 작은 수를 다시 넣는다. O(logN)
-* 9. 이 과정을 반복하며 최소값을 찾는다. O(N)
-*	--> O(NlogN)
-*/
