911b98ceb20e4e1bbe7a013f496799542fa17a51
Kim Gihong <kihong2424@gmail.com>
2024-07-07 08:25:34 +0900

    Title: 연결리스트 1, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240707/연결리스트 1/linked-list1.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    string data;
+    Node *prev, *next;
+    Node(string data) :
+        data(data),
+        prev(nullptr),
+        next(nullptr) {}
+};
+
+void insert_next(Node *u, Node *singleton) {
+    singleton->prev = u;
+    singleton->next = u->next;
+
+    if (nullptr != singleton->prev)
+        singleton->prev->next = singleton;
+    if (nullptr != singleton->next)
+        singleton->next->prev = singleton;
+}
+
+void insert_prev(Node *u, Node *singleton) {
+    singleton->prev = u->prev;
+    singleton->next = u;
+
+    if (nullptr != singleton->prev)
+        singleton->prev->next = singleton;
+    if (nullptr != singleton->next)
+        singleton->next->prev = singleton;
+}
+
+void pop(Node *u) {
+    if (nullptr != u->prev)
+        u->prev->next = u->next;
+    if (nullptr != u->next)
+        u->next->prev = u->next;
+    
+    u->prev = u->next = nullptr;
+}
+
+void    ft_print(Node *cur)
+{
+    if (cur->prev)
+        cout << cur->prev->data;
+    else
+        cout << "(Null)";
+    
+    cout << ' ' << cur->data << ' ';
+
+    if (cur->next)
+        cout << cur->next->data;
+    else
+        cout << "(Null)";
+    cout << '\n';
+}
+
+int main() {
+    string s_init;
+    cin >> s_init;
+
+    Node *cur = new Node(s_init);
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int cmd;
+        string s_val;
+        cin >> cmd;
+        if (cmd == 1)
+        {
+            cin >> s_val;
+            Node *new_n = new Node(s_val);
+            insert_prev(cur, new_n);
+        }
+        else if (cmd == 2)
+        {
+            cin >> s_val;
+            Node *new_n = new Node(s_val);
+            insert_next(cur, new_n);
+        }
+        else if (cmd == 3)
+            cur = cur->prev;
+        else if (cmd == 4)
+            cur = cur->next;
+        ft_print(cur);
+    }
+
+    return 0;
+}
